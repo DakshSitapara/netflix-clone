e0b09362e3664a5ace05b51c20bb8baa4f51ce6a
DakshSitapara <dakshsitapara@gmail.com>
2025-08-12 15:56:16 +0530

    info buttone fix

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useRouter } from "next/router";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoritesButton from "./FavoritesButton";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
+import { BiChevronDown } from "react-icons/bi";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -10,6 +12,8 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
 
   const router = useRouter();
+  const { openModal } = useInfoModalStore();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -34,6 +38,11 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30} />
             </div>
               <FavoritesButton movieId={data?.id} />
+              <div
+                  onClick={() => openModal(data?.id)}
+                  className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+                <BiChevronDown size={30} className="text-white group-hover/item:text-neutral-300" />
+              </div>
           </div>
           <p className="text-green-400 font-semibold mt-4 text-sm lg:text-base">{data.title}</p>
           <div className="flex flex-row items-center gap-1 mt-2">
